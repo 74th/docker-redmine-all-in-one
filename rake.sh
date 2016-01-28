7b25f00b741f95e498a696b8ed4c0d5a6234e2bf
74th <site@j74th.com>
2016-01-29 07:43:47 +0900

    refactor

--- rake.sh
+++ rake.sh
@@ -9,9 +9,10 @@ cd /var/lib/redmine/
 sudo -u www-data bundle exec rake generate_secret_token
 sudo -u www-data RAILS_ENV=production bundle exec rake db:migrate
 # plugins
-sudo -u www-data RAILS_ENV=production bundle exec rake redmine:plugins:migrate RAILS_ENV=production NAME=redmine_scm
-sudo -u www-data RAILS_ENV=production bundle exec rake redmine:plugins:migrate RAILS_ENV=production NAME=redmine_backlogs
-sudo -u www-data RAILS_ENV=production bundle exec rake redmine:plugins:migrate RAILS_ENV=production NAME=redmine_issue_templates
-sudo -u www-data RAILS_ENV=production bundle exec rake redmine:plugins:migrate RAILS_ENV=production NAME=redmine_code_review
+sudo -u www-data RAILS_ENV=production NAME=redmine_scm bundle exec rake redmine:plugins:migrate
+sudo -u www-data RAILS_ENV=production NAME=redmine_backlogs bundle exec rake redmine:plugins:migrate
+sudo -u www-data RAILS_ENV=production NAME=redmine_issue_templates bundle exec rake redmine:plugins:migrate
+sudo -u www-data RAILS_ENV=production NAME=redmine_code_review bundle exec rake redmine:plugins:migrate
+sudo -u www-data RAILS_ENV=production NAME=clipboard_image_paste bundle exec rake redmine:plugins:migrate
 
 service postgresql stop
